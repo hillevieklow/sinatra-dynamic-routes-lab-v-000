5012ac7817c6fb18b58457c9064590bcb80ea084
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-11 14:46:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ class App < Sinatra::Base
   get '/square/:number' do
     @number = params[:number].to_i
     "#{@number**2}"
-
   end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase].to_s
+    "#{@phrase.times(@number)}"
+  end
+
 end
