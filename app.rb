701e9cbd4e029949c025465482fec526890ea813
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-11 15:10:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,8 +29,17 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     @operation = params[:operations]
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
 
+    case @operations.downcase
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number1 - @number2}"
+    when "multuply"
+      "#{@number1 * @number2}"
+    when "divide"
+      "#{@number1 / @number2}"
   end
 end
