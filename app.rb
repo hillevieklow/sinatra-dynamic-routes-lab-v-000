57764c855a20670a0a8a1cb452655ea9031286aa
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-11 15:08:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,4 +26,11 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operations]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+
+  end
 end
